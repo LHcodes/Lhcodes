87b0993938196a8831ed4ee6a33ebc9783e667d5
Luiz Henrique Silva <94072600+SilvaByte@users.noreply.github.com>
2023-12-11 23:42:59 -0300

    Finalizado o header mf

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,16 @@
+import Logo from '../../../public/img/logo/footer.svg'
+import style from './Footer.module.css'
+
+function Footer() {
+    return(
+        <div className={style.footer}>
+        <div className={style.logo}>
+            <img src={Logo} alt="" />
+        </div>
+        <div>
+            <p>© 2023 LHcodes.com</p>
+        </div>
+        </div>
+    )
+}
+export default Footer
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,20 @@
+import Logo from '../../../public/img/logo/meuLogo.svg'
+import icon from '../../../public/img/icon/menu.svg'
+
+import style from './Header.module.css'
+
 
 function Header() {
     return ( 
-        <div>teste</div>
+        <div className={style.header}>
+            <div className={style.logo} >
+                <img src={Logo} alt="" />
+            </div>
+
+            <div className={style.icon} >
+                <img src={icon} alt="" />
+            </div>
+        </div>
     )
 }
 export default Header
